d861889642fbdd72faa006d2552d21102e8c19fe
atahar <ataharsk10@gmail.com>
2024-04-15 00:45:21 +0530

    Readme Modified

--- README.md
+++ README.md
@@ -3,41 +3,68 @@
 ## Problem Overview
 Python program that takes a JSON file (which contains the information of a directory in nested structure) and prints out its content in the console in the style of `ls` (Linux utility).
 
-# Implement the command `ls`. The program should parse the JSON file and print the top-level directories and files.
+## The program should parse the JSON file and print the top-level directories and files.
 ```bash
-abc
+python -m pyls
+```
+## Prints all files and directories, including those starting with `.`.
+```bash
+python -m pyls -A
+```
+## Prints the results vertically with additional information like permissions, size, and time modified.
+```bash
+python -m pyls -l
+```
+## Prints the results in reverse order.  -------
+```bash
+python -m pyls -l -r
+```
+## Prints the results sorted by `time_modified` (oldest first).
+```bash
+python -m pyls -l -r -t
+```
+## Filter with `file` and `dir`.
+```bash
+python -m pyls -l -r -t --filter=dir
+```
+```bash
+python -m pyls -l -r -t --filter=file
+```
+## Handle navigating the structure within the JSON using paths.
+```bash
+python -m pyls -l parser
+```
+```bash
+python -m pyls -l parser/parser.go
+```
+## Show human-readable size.
+```bash
+python -m pyls -h parser
+```
+## Option to print a helpful message.
+```bash
+python -m pyls --help
+```
+## To install this package as "pyls"
+```bash
+pip install -e . --user
 ```
 
-# Subtask 2: ls -A (2 points)
-# Implement the argument `-A`, which prints all files and directories, including those starting with `.`.
+# To Run This Project
 
-# Subtask 3: ls -l (10 points)
-# Implement the argument `-l`, which prints the results vertically with additional information like permissions, size, and time modified.
-
-# Subtask 4: ls -l -r (3 points)
-# Implement the argument `-r`, which prints the results in reverse order.
-
-# Subtask 5: ls -l -r -t (5 points)
-# Implement the argument `-t` that prints the results sorted by `time_modified` (oldest first).
-
-# Subtask 6: ls -l -r -t --filter=<option> (5 points)
-# Implement the argument `--filter=<option>`, where available options are `file` and `dir`.
-
-# Subtask 7: Handle Paths (5 points)
-# Handle navigating the structure within the JSON using paths.
-
-# Subtask 8: ls -h (5 points)
-# Show human-readable size.
-
-# Subtask 9: ls --help (5 points)
-# Implement `--help` option to print a helpful message.
-
-
-
-# conda create -n env_name python=3.12.2 -y
-# conda env list
-# conda activate env_name
-# conda remove -n env_name --all
-# pip install -e . --user #can show change real time
-# python setup.py install
-# pip install .
+## Create Virtual Environment
+```bash
+conda create -n env_name python=3.12.2 -y
+```
+## Activate Virtual Environment
+```bash
+conda activate env_name
+```
+## Install Dependency
+```bash
+pip install -rrequirements.txt
+```
+## Install Setup [Optional]
+```bash
+python setup.py install
+```
--- pyls/__main__.py
+++ pyls/__main__.py
@@ -2,24 +2,64 @@ from pyls.module import parse_json,handle_path,ls,display
 import argparse
 
 # Function Expand Arguments
-def expand():
+def expand() -> None:
     """
     This function will take the arguments, and parsed it accordingly,
     Call the associated arguments module form module.py,
     Raise an error if occurred.
     """
     # Assignment:Argument Handling
-    parser = argparse.ArgumentParser(description="Python implementation of ls command")
-    parser = argparse.ArgumentParser(conflict_handler="resolve")
-    parser.add_argument('path', nargs='?', default='.', help="Path to directory")
-    parser.add_argument('--dirpath', default='data/structure.json',help="Path for the directory")
-    parser.add_argument('-A', action='store_true', help="Show hidden files")
-    parser.add_argument('-l', action='store_true', help="Use a long listing format")
-    parser.add_argument('-r', action='store_true', help="Reverse the order of the sort")
-    parser.add_argument('-t', action='store_true', help="Sort by time modified")
-    parser.add_argument('--filter', help="Filter by file[--filter==file] or directory[--filter==dir]") #,choices=['file', 'dir']
-    parser.add_argument('-h',action='store_true',help='Print human-readable sizes')
-    parser.add_argument("--help", action='help',help='Find Help')
+    parser = argparse.ArgumentParser(
+        description="Python implementation of ls command"
+        )
+    parser = argparse.ArgumentParser(
+        conflict_handler="resolve"
+        )
+    parser.add_argument(
+        'path', 
+         nargs='?',
+         default='.',
+        help="Path to directory"
+    )
+    parser.add_argument(
+        '--dirpath',
+        default='data/structure.json',
+        help="Path for the directory"
+        )
+    parser.add_argument(
+        '-A',
+        action='store_true',
+        help="Show hidden files"
+        )
+    parser.add_argument(
+        '-l',
+        action='store_true',
+        help="Use a long listing format"
+        )
+    parser.add_argument(
+        '-r',
+        action='store_true',
+        help="Reverse the order of the sort"
+        )
+    parser.add_argument(
+        '-t',
+        action='store_true',
+        help="Sort by time modified"
+        )
+    parser.add_argument(
+        '--filter',
+        help="Filter by file[--filter==file] or directory[--filter==dir]"
+        ) #,choices=['file', 'dir']
+    parser.add_argument(
+        '-h',
+        action='store_true',
+        help='Print human-readable sizes'
+        )
+    parser.add_argument(
+        "--help", 
+        action='help',
+        help='Find Help'
+        )
     args = parser.parse_args()
 
     # Assignment:Extract argument
@@ -38,9 +78,20 @@ def expand():
     current_directory = handle_path(path,data)
     if current_directory is not None:
         # Linux ls commands
-        files = ls(current_directory,show_hidden,use_long_format)
+        files = ls(
+            current_directory,
+            show_hidden,
+            use_long_format
+            )
         # Display
-        display(files,use_long_format,reverse_order,sort_by_time,filter_option,human_readable)
+        display(
+            files,
+            use_long_format,
+            reverse_order,
+            sort_by_time,
+            filter_option,
+            human_readable
+            )
 
 if __name__ == "__main__":
     expand()
